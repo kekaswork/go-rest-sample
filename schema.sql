c6951cc8f0079d12fcef037c972c77ea8228da09
almaz <almaz@dreamersofday.co>
2024-05-29 20:21:54 +0200

    some sample data

--- schema.sql
+++ schema.sql
@@ -19,3 +19,18 @@ CREATE TABLE IF NOT EXISTS Marks (
     FOREIGN KEY (subject_id) REFERENCES Subjects(id)
 );
 
+-- Inserting sample data rows
+INSERT INTO Students (first_name, last_name) VALUES ('Almaz', 'Galimianov');
+INSERT INTO Students (first_name, last_name) VALUES ('Kate', 'Galimianova');
+INSERT INTO Students (first_name, last_name) VALUES ('Ivan', 'Pupkin');
+
+INSERT INTO Subjects (name) VALUES ('Math');
+INSERT INTO Subjects (name) VALUES ('Physics');
+INSERT INTO Subjects (name) VALUES ('Programming');
+
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (1, 1, 4, '2023-05-01');
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (1, 2, 3, '2023-05-02');
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (2, 2, 5, '2023-05-03');
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (2, 3, 2, '2023-05-04');
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (3, 1, 1, '2023-05-05');
+INSERT INTO Marks (student_id, subject_id, mark, mark_date) VALUES (3, 3, 4, '2023-05-06');
